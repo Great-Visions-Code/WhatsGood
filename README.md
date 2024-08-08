c8d78a0a86f03082d8c81f337ef5448dedf4fafb
Gustavo Vazquez <great.visions@icloud.com>
2024-08-08 14:25:39 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# WhatsGood
+ Gratefulness App
---
+++ WhatsGood/WhatsGoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsGoodApp.swift
+//  WhatsGood
+//
+//  Created by Gustavo Vazquez on 8/8/24.
+//
+
+import SwiftUI
+
+@main
+struct WhatsGoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
