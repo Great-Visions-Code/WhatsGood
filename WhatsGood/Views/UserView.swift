8b89b658f35be7eff763395eba0a56f2b45273ee
Gustavo Vazquez <great.visions@icloud.com>
2024-08-12 17:26:40 -0700

    Create temp views UserRegistrationView and UserVIew

---
+++ WhatsGood/Views/UserView.swift
@@ -0,0 +1,18 @@
+//
+//  UserView.swift
+//  WhatsGood
+//
+//  Created by Gustavo Vazquez on 8/12/24.
+//
+
+import SwiftUI
+
+struct UserView: View {
+    var body: some View {
+        Text("Placeholder for UserView()")
+    }
+}
+
+#Preview {
+    UserView()
+}
